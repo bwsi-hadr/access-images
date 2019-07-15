b87a36f243392a29eb4874dc3950c9f5457ce30e
Gabi Tessier <mtessier246@gmail.com>
2019-07-15 15:20:11 -0400

    even more on databases!

--- RemoteSensingDB.py
+++ RemoteSensingDB.py
@@ -6,6 +6,7 @@ from bson import objectid
 #uploadphoto , downloadphoto
 class RemSensDB():
     db = None
+    fs = None
 
     #creats the database
     def DataBaseInitialize(self):
@@ -32,36 +33,37 @@ class RemSensDB():
         b = self.db["raw_images"].find_one({"date": d})
         #print(b)
 
+    # store the data in the database. Returns the id of the file in gridFS
     def uploadphoto(self, b):
-
-        fs = gridfs.GridFS(self.db)
+        #storing data
+        self.fs = gridfs.GridFS(self.db)
 
         with open(b, 'rb') as b:
-            store = fs.put(b)
+            store = self.fs.put(b)
 
-        outputdata = fs.get(store).read()
-        #print(outputdata)
+        #print("store:   ", store)
+        return store
+
+# create an output file and store the image in the output file
+    def downloadphoto(self, a):
+        #retrieving data and returning .jpg in bytes
+        outputdata = self.fs.get(a).read()
         return outputdata
-
-
-    def downloadphoto(self):
-        pass
-
-
 
     def __init__(self):
         self.DataBaseInitialize()
         #self.insertData()
 
 if __name__ == "__main__":
-    """json = [
+    #-------------------TESTING VERIABLES ----------------------------------------
+    json = [
     {"name": "C:/Users/Gabi/Documents/2019-07-12/image1.jpg", "date": "946688580"},
     {"name": "C:/Users/Gabi/Documents/2019-07-12/image2.jpg", "date": "946692180"},
     {"name": "C:/Users/Gabi/Documents/2019-07-12/image3.jpg", "date": "946796400"},
     {"name": "C:/Users/Gabi/Documents/2019-07-12/image4.jpg", "date": "949482000"},
     {"name": "C:/Users/Gabi/Documents/2019-07-12/image5.jpg", "date": "949564800"},
     {"name": "C:/Users/Gabi/Documents/2019-07-12/image6.jpg", "date": "954820800"}
-    ]"""
+    ]
     query = "Image1.jpg"
     na = 'C:/Users/Gabi/Documents/2019-07-12/Image4.jpg'
     da = '954820800'
@@ -71,10 +73,13 @@ if __name__ == "__main__":
     filename = "image1.jpg"
 
     dbMan = RemSensDB()
+
+    #----------METHODS------------------------
+
     #dbMan.insertData(json)
     #dbMan.queryDB(query)
     #dbMan.findByName(na)
     #dbMan.findByDate(da)
     #dbMan.findByID(id)
-    #dbMan.uploadphoto(filename)
-    dbMan.downloadphoto(filename)
+    dbMan.uploadphoto(filename)
+    #dbMan.downloadphoto(dbMan.uploadphoto(filename))

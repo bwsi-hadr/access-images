d9cf39ec8e9b1a817e88716bf16253bbfae0e46d
Gabi Tessier <mtessier246@gmail.com>
2019-07-19 09:54:45 -0400

    database is working

--- RemoteSensingDB.py
+++ RemoteSensingDB.py
@@ -1,14 +1,17 @@
+#Gabi Tessier, 7/19/2019
+#This creates a database with MongoDB with PyMongo
 import pymongo
 from bson.objectid import ObjectId
 import gridfs
 from bson import objectid
+
 
 #uploadphoto , downloadphoto
 class RemSensDB():
     db = None
     fs = None
 
-    #creates the database
+    #creats the database
     def DataBaseInitialize(self):
         client = pymongo.MongoClient()
         self.db = client["database"]
@@ -17,32 +20,29 @@ class RemSensDB():
     #inserts data into the database from the json file
     def insertData(self, js):
         self.db["raw_images"].insert_many(js)
-        return 0
 
     #finds the object from a given id
     def findByID(self, i):
         o = self.db["raw_images"].find_one({"_id": ObjectId(i)})
-        return o
+        #print(o)
 
     #finds the object from a given name
     def findByName(self, n):
         j = self.db["raw_images"].find_one({"name": n})
-        return j
-
+        print(j)
 
     #find the object from a given date
-#    def findByDate(self, d):
-#        b = self.db["raw_images"].find_one({"date": d})
-#        return b
-
+    def findByDate(self, d):
+        b = self.db["raw_images"].find_one({"date": d})
+        #print(b)
 
     # store the data in the database. Returns the id of the file in gridFS
-    def uploadphoto(self, b):
+    def uploadphoto(self, b, name):
         #storing data
         self.fs = gridfs.GridFS(self.db)
-
+        #print(name)
         with open(b, 'rb') as b:
-            store = self.fs.put(b)
+            store = self.fs.put(b, filename = name)
         #print("store:   ", store)
         return store
 
@@ -58,31 +58,14 @@ class RemSensDB():
 
 if __name__ == "__main__":
 
-    #-------------------TESTING VERIABLES ----------------------------------------
-
-    json = [
-    {"name": "images/image1.jpg", "date": "946688580"},
-    {"name": "images/image2.jpg", "date": "946692180"},
-    {"name": "images/image3.jpg", "date": "946796400"},
-    {"name": "images/image4.jpg", "date": "949482000"},
-    {"name": "images/image5.jpg", "date": "949564800"},
-    {"name": "images/image6.jpg", "date": "954820800"}
-    ]
-    query = "Image1.jpg"
-    na = 'images/image4.jpg'
-    da = '954820800'
-    id = '5d28d5aa6c5112c81e28a653'
-
-    #info for uploadphoto()
-    filename = "images/image1.jpg"
-
     dbMan = RemSensDB()
 
-    #----------METHODS------------------------
+    #-------------------METHODS------------------------#
 
-    #dbMan.insertData(json)
-    #dbMan.findByName(na)
-    #dbMan.findByDate(da)
-    #dbMan.findByID(id)
-    #dbMan.uploadphoto(filename)
+    dbMan.insertData(file)
+    dbMan.queryDB(query)
+    dbMan.findByName(na)
+    dbMan.findByDate(da)
+    dbMan.findByID(id)
+    dbMan.uploadphoto(filename)
     dbMan.downloadphoto(dbMan.uploadphoto(filename))
--- downloadIm.py
+++ downloadIm.py
@@ -1,18 +1,23 @@
+#Gabi Tessier, 7/19/2019
+#this program asks usr for directory path and will insert image and name into database
 import RemoteSensingDB as dmain
 from PIL import Image
 import glob
 import os
 import pathlib
 
-pathname = input("Enter the path to the directory: ")
+
+pathname = input("Enter the path to the directory (ex format: images/): ")
 
 path = pathlib.Path(pathname)
 file_list = [x for x in path.iterdir() if (x.suffix.lower() == '.jpg')]
 #print(file_list)
-image_list=[]
+
+image_list = {}
+
+#quiry filename and make sure doesnt exist befor inserting
 for filename in file_list:
-    im=Image.open(filename)
-    image_list.append({"name": filename.stem, "object": im})
+    dmain.RemSensDB().uploadphoto(filename, filename.stem)
 
 #print(image_list)
 print("Files are Successfully uploaded to Database.")
